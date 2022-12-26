20ce0dc12d71d18db2fc032c7404e83b8a361cdc
LiminalCrab <preston@sudogami.com>
2022-12-26 13:39:44 -0800

    go-flags package

--- test.go
+++ test.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, Arch!")
+	fmt.Println("Hello, Arch!")
+
+	type Options struct {
+		Verbose []bool `short:"v" long:"verbose" description:"Show verbose debug information"`
+	}
 }
