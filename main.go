bd5d0ce2ed9d239e67f07eacb5cd958d63b271b5
LiminalCrab <preston@sudogami.com>
2022-12-27 19:41:36 -0800

    docker go api, zerlog, cobra, httpheaders

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"net/http"
+
+	dockerClient "github.com/docker/docker/client"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	var hatectl bool
+	var run = &cobra.Command{
+		Use:   "run",
+		Short: "run docker container",
+		Long:  "run docker container but longer phrasing",
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Info().Msg("Test")
+			httpHeaders := map[string]string{}
+			host := ""
+			client := &http.Client{}
+			version := ""
 
-	flag.BoolVar(&hatectl, "hctl", false, "WHY ARE YOU DOING THIS")
-	flag.Parse()
-	fmt.Println(hatectl)
+			dc, err := dockerClient.NewClient(host, version, client, httpHeaders)
+			if err != nil {
+				log.Error().Err(err)
+				return
+			}
+			defer dc.Close()
+
+		},
+	}
+
+	run.Execute()
+
 }
