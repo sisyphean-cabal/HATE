ac4864b3f8ac328f5ffc10c1ee832eb1b1c3efff
LiminalCrab <preston@sudogami.com>
2022-12-27 20:50:59 -0800

    docker network interface, http headers ect

--- main.go
+++ main.go
@@ -2,43 +2,52 @@ package main
 
 import (
 	"context"
-	"net/http"
+	"encoding/json"
+	"os"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	dockerClient "github.com/docker/docker/client"
-	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	var run = &cobra.Command{
-		Use:   "run",
-		Short: "run docker container",
-		Long:  "run docker container but longer phrasing",
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	log := zerolog.New(output).With().Timestamp().Logger()
+	list := &cobra.Command{
+		Use:   "list",
+		Short: "list docker containers",
+		Long:  "list docker containers, butt longer",
 		Run: func(cmd *cobra.Command, args []string) {
 			log.Info().Msg("Test")
-			httpHeaders := map[string]string{}
-			host := "localhost"
-			client := &http.Client{}
-			version := ""
+			host := "tcp://0.0.0.0:5555"
 
 			dc, err := dockerClient.NewClientWithOpts(
-				dockerClient.WithHTTPClient(client),
-				dockerClient.WithHTTPHeaders(httpHeaders),
 				dockerClient.WithHost(host),
-				dockerClient.WithVersion(version),
+				dockerClient.WithAPIVersionNegotiation(),
 			)
 			if err != nil {
-				log.Error().Err(err)
+				log.Error().Err(err).Msg("Error while creating docker client")
 				return
 			}
 			defer dc.Close()
 
 			containers, err := dc.ContainerList(context.Background(), types.ContainerListOptions{})
+			if err != nil {
+				log.Error().Err(err).Msg("Error while getting containers")
+				return
+			}
+			d, err := json.Marshal(containers)
+			if err != nil {
+				log.Error().Err(err).Msg("Error while marshalling container list")
+				return
+			}
+			log.Info().RawJSON("containers", d).Msg("here are the containers")
 
 		},
 	}
 
-	run.Execute()
+	list.Execute()
 
 }
