3a82a1e45aa165f43012747e6ee1f173c117953b
LiminalCrab <preston@sudogami.com>
2022-12-27 18:07:09 -0800

    flag module test

---
+++ docs/resources.md
@@ -0,0 +1,6 @@
+### Kubernetes
+Kubernetes in 5 mins 
+https://www.youtube.com/watch?v=PH-2FfFD2PU
+
+### Go
+https://go.dev/doc/
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Are we there yet?")
+	var hatectl bool
 
+	flag.BoolVar(&hatectl, "hctl", false, "WHY ARE YOU DOING THIS")
+	flag.Parse()
+	fmt.Println(hatectl)
 }
