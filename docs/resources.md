187ae9a0fea640aafba13f4bac618a9821d0afe4
LiminalCrab <preston@sudogami.com>
2022-12-28 16:56:39 -0800

    config file

--- docs/resources.md
+++ docs/resources.md
@@ -1,12 +1,36 @@
+### Objective
+Map containers to resources
+- Find resources, how to reach them
+- Execute commands on those. (for each one of these things, we need two of those things. Resource usage)
+
+
+Get a container running using this
+- Stand up containers
+- Bring down containers
+- List the containers
+- Get it running on multiple hosts A list of host strings.
+- Define multiple hosts, run list on them ect. Or run start or stop on a given host. Given docker metadata.
+
+- Be able to tag each container, that allows us to associate metadata and look it up based on that.
+
 ### Kubernetes
 Kubernetes in 5 mins 
 https://www.youtube.com/watch?v=PH-2FfFD2PU
 
 ### Go
 https://go.dev/doc/
+https://stackoverflow.com/questions/tagged/go
+https://hashnode.com/n/go
+https://forum.golangbridge.org/
+
+
+#### yaml.v3
+https://pkg.go.dev/gopkg.in/yaml.v3?utm_source=godoc
+
 
 ### Docker Go API
 https://pkg.go.dev/github.com/docker/docker/client
+
 
 ## Questions
 
--- main.go
+++ main.go
@@ -10,39 +10,63 @@ import (
 	"github.com/docker/docker/api/types"
 	dockerClient "github.com/docker/docker/client"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 )
 
-
-func main() {
-	hcfg, err := nConn(*cInfo, hateConfig{
-		Protocol: "tcp://",
-		Host: "0.0.0.0",
-		Port: "5555",
-	})
-	if err != nil {
-		log.Fatal().Err(err).Msg("A fatal error has occurred.")
+type Config struct {
+	Conn struct {
+		Protocol string `yaml:"protocol"`
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
 	}
-	defer hcfg.close()
-	hConn = fmt.Sprintf("%s%s%s", &hcfg.Protocol, &hcfg.Host, &hcfg.Port)
+}
+
+func HConfig(params ...string) Config {
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
 	log := zerolog.New(output).With().Timestamp().Logger()
+
+	file, err := os.Open(`hate.yml`)
+
+	//TODO: Check if the file exists, if it doesn't make it.
+	var hcfg Config
+
+	if err != nil {
+		log.Error().Err(err).Msg("Either the hate config is missing or corrupt. Please check your path.")
+	}
+
+	defer file.Close()
+
+	if file != nil {
+		decoder := yaml.NewDecoder(file)
+		if err := decoder.Decode(&hcfg); err != nil {
+			log.Error().Err(err).Msg("An error occurred while decoding configuration file.")
+		}
+	}
+
+	return hcfg
+
+}
+
+func main() {
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	log := zerolog.New(output).With().Timestamp().Logger()
+
+	hconn := fmt.Sprintf("%s%s:%s", HConfig().Conn.Protocol, HConfig().Conn.Host, HConfig().Conn.Port)
 	list := &cobra.Command{
 		Use:   "list",
 		Short: "list docker containers",
 		Long:  "list docker containers, butt longer",
 		Run: func(cmd *cobra.Command, args []string) {
 			log.Info().Msg("Test")
-			host := hConn 
+			host := hconn
 
 			dc, err := dockerClient.NewClientWithOpts(
 				dockerClient.WithHost(host),
 				dockerClient.WithAPIVersionNegotiation(),
 			)
+			log.Error().Err(err).Msg("Error while creating docker client")
 			if err != nil {
-				log.Error().Err(err).Msg("Error while creating docker client")
 				return
 			}
 			defer dc.Close()
@@ -60,18 +84,6 @@ func main() {
 			log.Info().RawJSON("containers", d).Msg("here are the containers")
 
 		},
-
-		start := &cobra.Command{
-			Use: "start",
-			Short: "start a docker container",
-			Long: "start docker containers but longer.",
-			Run: func(cmd, *cobra.Command, args []string) {
-				log.Info().Msg("Test")
-				host := "tcp://0.0.0.0:5555"
-			}
-		}
 	}
-
 	list.Execute()
-
 }
