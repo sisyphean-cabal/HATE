8bef04505bfd7389d02d0a4b7d489bb77dbfd712
LiminalCrab <preston@sudogami.com>
2022-12-28 17:32:40 -0800

    start command

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"os"
 	"time"
 
@@ -11,48 +10,14 @@ import (
 	dockerClient "github.com/docker/docker/client"
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
 )
 
-type Config struct {
-	Conn struct {
-		Protocol string `yaml:"protocol"`
-		Host     string `yaml:"host"`
-		Port     string `yaml:"port"`
-	}
-}
-
-func HConfig(params ...string) Config {
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	log := zerolog.New(output).With().Timestamp().Logger()
-
-	file, err := os.Open(`hate.yml`)
-
-	//TODO: Check if the file exists, if it doesn't make it.
-	var hcfg Config
-
-	if err != nil {
-		log.Error().Err(err).Msg("Either the hate config is missing or corrupt. Please check your path.")
-	}
-
-	defer file.Close()
-
-	if file != nil {
-		decoder := yaml.NewDecoder(file)
-		if err := decoder.Decode(&hcfg); err != nil {
-			log.Error().Err(err).Msg("An error occurred while decoding configuration file.")
-		}
-	}
-
-	return hcfg
-
-}
+var hconn = "tcp://0.0.0.0:5555"
 
 func main() {
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
 	log := zerolog.New(output).With().Timestamp().Logger()
 
-	hconn := fmt.Sprintf("%s%s:%s", HConfig().Conn.Protocol, HConfig().Conn.Host, HConfig().Conn.Port)
 	list := &cobra.Command{
 		Use:   "list",
 		Short: "list docker containers",
