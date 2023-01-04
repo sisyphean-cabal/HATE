351d95b67c69b7c0e8f4bbc59bd22228fb49459b
LiminalCrab <preston@sudogami.com>
2023-01-03 22:10:07 -0800

    channels, goroutines, and pain

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/docker/docker/api/types"
@@ -11,97 +13,174 @@ import (
 	dockerClient "github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
-var hconn = "tcp://0.0.0.0:5555"
+
+type errorMessage struct {
+	err error
+	reason string
+}
+
+
+func dcList(hconn string, ctx context.Context, outChan chan string, errChan chan errorMessage){
+		outChan <- "Starting list containers command"
+		host := hconn
+		fmt.Println("1")
+		dc, err := dockerClient.NewClientWithOpts(
+			dockerClient.WithHost(host),
+			dockerClient.WithAPIVersionNegotiation(),
+		)
+
+		if err != nil {
+			errChan <- errorMessage{
+				err,
+				"Error while creating docker client",
+			}
+			return
+		}
+		fmt.Println("2")
+
+		defer dc.Close()
+		containers, err := dc.ContainerList(ctx, types.ContainerListOptions{})
+		if err != nil {
+		fmt.Println("nope")
+			errChan <- errorMessage{
+				err,
+				"Error while getting containers",
+			}
+			return
+		}
+		fmt.Println("3")
+
+		d, err := json.Marshal(containers)
+		if err != nil {
+			errChan <- errorMessage{
+				err,
+				"failed marshalling",
+			}
+			return
+		}
+		fmt.Println("4")
+
+		outChan <- string(d)
+}
+
+func dcCreate(hconn string, ctx context.Context, outChan chan string, errChan chan errorMessage){
+		outChan <- "Starting create"
+		host := hconn
+
+		dc, err := dockerClient.NewClientWithOpts(
+			dockerClient.WithHost(host),
+			dockerClient.WithAPIVersionNegotiation(),
+		)
+
+		if err != nil {
+			errChan <- errorMessage{
+				err,
+				"error when starting docker container",
+			}
+			return
+		}
+
+		defer dc.Close()
+
+		resp, err := dc.ContainerCreate(ctx, &container.Config{
+			Image: "alpine",
+			Cmd:   []string{"echo", "hello world"},
+			Tty:   false,
+		}, nil, nil, nil, "")
+
+		if err != nil {
+			errChan <- errorMessage{
+				err,
+				"failed to get start docker container",
+			}
+			return
+		}
+		if err := dc.ContainerStart(ctx,
+			resp.ID, types.ContainerStartOptions{}); err != nil {
+			log.Error().Err(err).Msg("Seriously what the fuck?")
+		}
+		stats, errstat := dc.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+		select {
+		case err := <-errstat:
+			if err != nil {
+				errChan <- errorMessage{
+					err,
+					"failed to wait for container.",
+				}
+				return
+			}
+		case <-stats:
+		}
+
+		out, err := dc.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+		if err != nil {
+			errChan <- errorMessage{
+				err,
+				"failed to get container logs",
+			}
+			return
+		}
+		stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+}
 
 func main() {
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	log := zerolog.New(output).With().Timestamp().Logger()
+
+	// output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+
+	zerolog.TimeFieldFormat = time.RFC3339
+	outChan := make(chan string)
+	errChan := make(chan errorMessage)
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() {
+		for line := range outChan {
+			log.Log().Msg(line)
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for errorMsg := range errChan {
+			fmt.Println("got one")
+			log.Log().Err(errorMsg.err).Msg(errorMsg.reason)
+		}
+		wg.Done()
+	}()
+
+	hconn := "tcp://0.0.0.0:2375"
+	ctx := context.Background()
 
 	list := &cobra.Command{
 		Use:   "list",
 		Short: "list docker containers",
 		Long:  "list docker containers, butt longer",
 		Run: func(cmd *cobra.Command, args []string) {
-			log.Info().Msg("Test")
-			host := hconn
-
-			dc, err := dockerClient.NewClientWithOpts(
-				dockerClient.WithHost(host),
-				dockerClient.WithAPIVersionNegotiation(),
-			)
-			log.Error().Err(err).Msg("Error while creating docker client")
-			if err != nil {
-				return
-			}
-			defer dc.Close()
-
-			containers, err := dc.ContainerList(context.Background(), types.ContainerListOptions{})
-			if err != nil {
-				log.Error().Err(err).Msg("Error while getting containers")
-				return
-			}
-			d, err := json.Marshal(containers)
-			if err != nil {
-				log.Error().Err(err).Msg("Error while marshalling container list")
-				return
-			}
-			log.Info().RawJSON("containers", d).Msg("here are the containers")
-
+			dcList(hconn, ctx, outChan, errChan)
 		},
 	}
-
+	
 	create := &cobra.Command{
 		Use:   "create",
 		Short: "create and start docker containers",
 		Long:  "create and start longer docker containers",
 		Run: func(cmd *cobra.Command, args []string) {
-			log.Info().Msg("Starting create")
-			host := hconn
-
-			dc, err := dockerClient.NewClientWithOpts(
-				dockerClient.WithHost(host),
-				dockerClient.WithAPIVersionNegotiation(),
-			)
-
-			if err != nil {
-				log.Error().Err(err).Msg("Error when starting docker container.")
-				return
-			}
-
-			defer dc.Close()
-
-			resp, err := dc.ContainerCreate(context.Background(), &container.Config{
-				Image: "alpine",
-				Cmd:   []string{"echo", "hello world"},
-				Tty:   false,
-			}, nil, nil, nil, "")
-
-			if err != nil {
-				log.Error().Err(err).Msg("why the fuck is our logging command so long?")
-			}
-			if err := dc.ContainerStart(context.Background(),
-				resp.ID, types.ContainerStartOptions{}); err != nil {
-				log.Error().Err(err).Msg("Seriously what the fuck?")
-			}
-			stats, errstat := dc.ContainerWait(context.Background(), resp.ID, container.WaitConditionNotRunning)
-			select {
-			case err := <-errstat:
-				if err != nil {
-					log.Error().Err(err).Msg("Writing logs has never been more painful")
-				}
-			case <-stats:
-			}
-
-			out, err := dc.ContainerLogs(context.Background(), resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-			if err != nil {
-				log.Error().Err(err).Msg("We need to fix this oh my god. We have to write these after almost ever statement. Jesus.")
-			}
-			stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+			dcCreate(hconn, ctx, outChan, errChan)
 		},
 	}
-	create.Execute()
-	list.Execute()
+
+	rootCmd := &cobra.Command{
+		Use: "h8ctl",
+	}
+	rootCmd.AddCommand(list, create)
+	rootCmd.Execute()
+	close(outChan)
+	close(errChan)
+	wg.Wait()
+	fmt.Println("done")
 }
